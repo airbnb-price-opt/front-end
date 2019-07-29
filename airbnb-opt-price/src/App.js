337a356fb94c7e6b1fb6aaca7e8576347f4b6224
anthonypiazza <piazza.anthony.n@gmail.com>
2019-07-29 14:32:20 -0500

    Register UI complete, added styled-components, form and image

--- airbnb-opt-price/src/App.js
+++ airbnb-opt-price/src/App.js
@@ -1,18 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import Register from './components/Register';
-import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
         <Register />
-        <img src={logo} className="App-logo" alt="logo" />
-        <Login />
-      </header>
     </div>
   );
 }
