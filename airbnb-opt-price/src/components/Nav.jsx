043e1b829323078e43d118f1ee4f55eb34fb1b8e
Eli Sacks <bastlifa@gmail.com>
2019-07-29 14:21:06 -0400

    Header and Nav

---
+++ airbnb-opt-price/src/components/Nav.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+import { NavDiv, StyledNavLink } from "../StyledComps"
+
+
+
+const Nav = () =>
+{
+    return (
+        <NavDiv>
+            <StyledNavLink to="/your-listings" activeClassName="active-nav">YOUR LISTINGS</StyledNavLink>
+            <StyledNavLink to="/register" activeClassName="active-nav">REGISTER</StyledNavLink>
+            <StyledNavLink to="/login" activeClassName="active-nav">LOGIN</StyledNavLink>
+            <StyledNavLink to="/logout" activeClassName="active-nav">LOGOUT</StyledNavLink>
+        </NavDiv>
+    )
+}
+
+export default Nav
