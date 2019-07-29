d43c98156dbea3dde1f4535add199daa9d05cca7
Arvin Agas <arvagas@gmail.com>
2019-07-29 14:40:38 -0400

    swapped file names

--- airbnb-opt-price/src/components/YourListings.js
+++ airbnb-opt-price/src/components/YourListings.js
@@ -1,29 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const YourListingsCard = ( { data } ) => {
+import YourListingsCard from './YourListingsCard'
+
+const UserHome = () => {
+
+    const [userListing, setUserListing] = useState([
+        {
+            img_url: 'https://images.unsplash.com/photo-1480074568708-e7b720bb3f09?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1353&q=80',
+            neighborhood: 'Albany, NY',
+            guests: '2',
+            min_nights: '5',
+            act_price: '$109',
+            opt_price: '$199',
+        },
+        {
+            img_url: 'https://images.unsplash.com/photo-1512917774080-9991f1c4c750?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+            neighborhood: 'Los Angeles, CA',
+            guests: '2',
+            min_nights: '2',
+            act_price: '$150',
+            opt_price: '$299',
+        },
+    ])
+
     return (
         <div>
-            {data.map(listing => (
-                <div>
-                    <img src={listing.img_url}></img>
-                    <div>
-                        <p>NEIGHBORHOOD: {listing.neighborhood}</p>
-                        <p>GUESTS: {listing.guests}</p>
-                        <p>MINIMUM NIGHTS: {listing.min_nights}</p>
-                        <p>ACTUAL PRICE: {listing.act_price}</p>
-                        <p>OPTIMAL PRICE: {listing.opt_price}</p>
-                    </div>
-                    <button>UPDATE LISTING</button>
-                    <button>TRASH</button>
-                </div>
-            ))}
-            
-            <div>
-                <p>ICON</p>
-                <p>ADD LISTING</p>
-            </div>
+            <h1>Your Listings</h1>
+            {console.log(userListing)}
+            <YourListingsCard data={userListing}/>
         </div>
     )
 }
 
-export default YourListingsCard
+export default YourListings
