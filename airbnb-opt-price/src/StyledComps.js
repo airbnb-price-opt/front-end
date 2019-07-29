afa75cec1ea6df1887dc6b1ada353c3bd6c10510
Eli Sacks <bastlifa@gmail.com>
2019-07-29 15:08:18 -0400

    login page

--- airbnb-opt-price/src/StyledComps.js
+++ airbnb-opt-price/src/StyledComps.js
@@ -1,9 +1,11 @@
 import styled from 'styled-components'
 import { NavLink } from 'react-router-dom'
+const appRed = '#eb5757';
+const appGrey = '#828282';
 
 export const NavDiv = styled.div`
     display: flex;
-    background-color: #828282;
+    background-color: ${appGrey};
     box-shadow: 3px 8px 18px rgba(130, 130, 130, 70);
     border:none;
     border-bottom-left-radius: 6px;
@@ -33,7 +35,70 @@ export const StyledHeader = styled.div`
     @import url('https://fonts.googleapis.com/css?family=Rock+Salt&display=swap');
     h1
     {
-        color: #eb5757;
+        color: ${appRed};
         font-family: 'Rock Salt', cursive;
+    }
+`;
+
+export const LoginContainerDiv = styled.div`
+    /* width: 80%; */
+    display: flex;
+    margin-top: 50px;
+    justify-content: space-around;
+    align-items: flex-end;
+    align-content: center;
+    
+`;
+
+export const LoginForm = styled.form`
+    background-color: ${appRed};
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    padding-left: 50px;
+    height: 315px;
+    width: 549px;
+    color: white;
+    input
+    {
+        width: 461px;
+        height: 34px;
+    }
+    label
+    {
+        margin-top: 40px;
+    }
+    button
+    {
+        background-color: white;
+        color: ${appRed};
+        box-shadow: 5px 5px 3px rgba(0, 0, 0, 0.2);
+        width: 459px;
+        height: 45px;
+        border-radius: 5px;
+        border: none;
+        margin-left: 3px;
+        margin-top: 40px;
+        font-size: 24px;
+    }
+`;
+
+export const LoginFormContainer = styled.div`
+    display: flex;
+    flex-flow: column nowrap;
+    align-items: center;
+    h2
+    {
+        color: ${appRed};
+        font-weight: normal;
+        font-size: 36px;
+        margin-bottom: 0px;
+    }
+    hr
+    {
+        border-color: ${appRed};
+        border-top: none;
+        width: 60%;
+        margin-bottom: 20px;
     }
 `;
