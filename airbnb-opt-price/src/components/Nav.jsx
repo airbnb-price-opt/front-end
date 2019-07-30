ad6d1962d3e1f0ac1db152ea0a0bb56cabbaa96d
Andrew Allen <30938535+AndrewA0112@users.noreply.github.com>
2019-07-30 18:09:24 -0500

    Listing forms (#45)
    * Console logging value changes for Add/Update
    
    * Mobile design for Add/Update
    
    * Syling to upload image
    
    * Register mobile design
    
    * Moved suspense tag
    
    * styled listing header
    
    * minor app wide stlying

--- airbnb-opt-price/src/components/Nav.jsx
+++ airbnb-opt-price/src/components/Nav.jsx
@@ -13,12 +13,29 @@ const Nav = () =>
         return pathname === "/"
     }
 
+    // if(localStorage.getItem('token') !== null && localStorage.getItem('token') !== '') {
+    //     return (
+    //         <NavDiv>
+    //             <StyledMobileNavLink to="/your-listings/" activeClassName="active-nav">YOUR LISTINGS</StyledMobileNavLink>
+    //             <StyledMobileNavLink to="/logout/" activeClassName="active-nav">LOGOUT</StyledMobileNavLink>
+    //         </NavDiv>
+    //     )
+    // }
+    // else {
+    //     return (
+    //             <NavDiv>
+    //                 <StyledMobileNavLink to="/register/" activeClassName="active-nav">REGISTER</StyledMobileNavLink>
+    //                 <StyledMobileNavLink to="/" activeClassName="active-nav" isActive={checkActive}>LOGIN</StyledMobileNavLink>
+    //             </NavDiv>
+    //     )
+    // }
+
     return (
         <NavDiv>
-            <StyledNavLink to="/your-listings/" activeClassName="active-nav">YOUR LISTINGS</StyledNavLink>
-            <StyledNavLink to="/register/" activeClassName="active-nav">REGISTER</StyledNavLink>
-            <StyledNavLink to="/" activeClassName="active-nav" isActive={checkActive}>LOGIN</StyledNavLink>
-            <StyledNavLink to="/logout/" activeClassName="active-nav">LOGOUT</StyledNavLink>
+            <StyledMobileNavLink to="/your-listings/" activeClassName="active-nav">YOUR LISTINGS</StyledMobileNavLink>
+            <StyledMobileNavLink to="/register/" activeClassName="active-nav">REGISTER</StyledMobileNavLink>
+            <StyledMobileNavLink to="/" activeClassName="active-nav" isActive={checkActive}>LOGIN</StyledMobileNavLink>
+            <StyledMobileNavLink to="/logout/" activeClassName="active-nav">LOGOUT</StyledMobileNavLink>
         </NavDiv>
     )
 }
