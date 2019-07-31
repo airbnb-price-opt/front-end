eb53ca5d18e3ec13b8d06d0b43f93617c6b7380b
Eli Sacks <bastlifa@gmail.com>
2019-07-31 19:56:48 -0400

    mobile nav dynamic too

--- airbnb-opt-price/src/components/MobileNav.jsx
+++ airbnb-opt-price/src/components/MobileNav.jsx
@@ -13,31 +13,31 @@ const Nav = () =>
         return pathname === "/"
     }
 
-    // if(localStorage.getItem('token') !== null) {
-    //     return (
-    //         <MobileNavDiv>
-    //             <StyledMobileNavLink to="/your-listings/" activeClassName="active-nav">YOUR LISTINGS</StyledMobileNavLink>
-    //             <StyledMobileNavLink to="/logout/" activeClassName="active-nav">LOGOUT</StyledMobileNavLink>
-    //         </MobileNavDiv>
-    //     )
-    // }
-    // else {
-    //     return (
-    //             <MobileNavDiv>
-    //                 <StyledMobileNavLink to="/register/" activeClassName="active-nav">REGISTER</StyledMobileNavLink>
-    //                 <StyledMobileNavLink to="/" activeClassName="active-nav" isActive={checkActive}>LOGIN</StyledMobileNavLink>
-    //             </MobileNavDiv>
-    //     )
-    // }
+    if(localStorage.getItem('token') !== null && localStorage.getItem('token') !== '') {
+        return (
+            <MobileNavDiv>
+                <StyledMobileNavLink to="/your-listings/" activeClassName="active-nav">YOUR LISTINGS</StyledMobileNavLink>
+                <StyledMobileNavLink to="/" onClick={_ => localStorage.removeItem('token')} activeClassName="active-nav">LOGOUT</StyledMobileNavLink>
+            </MobileNavDiv>
+        )
+    }
+    else {
+        return (
+                <MobileNavDiv>
+                    <StyledMobileNavLink to="/register/" activeClassName="active-nav">REGISTER</StyledMobileNavLink>
+                    <StyledMobileNavLink to="/" activeClassName="active-nav" isActive={checkActive}>LOGIN</StyledMobileNavLink>
+                </MobileNavDiv>
+        )
+    }
 
-    return (
-        <MobileNavDiv>
-            <StyledMobileNavLink to="/your-listings/" activeClassName="active-nav">YOUR LISTINGS</StyledMobileNavLink>
-            <StyledMobileNavLink to="/register/" activeClassName="active-nav">REGISTER</StyledMobileNavLink>
-            <StyledMobileNavLink to="/" activeClassName="active-nav" isActive={checkActive}>LOGIN</StyledMobileNavLink>
-            <StyledMobileNavLink to="/logout/" activeClassName="active-nav">LOGOUT</StyledMobileNavLink>
-        </MobileNavDiv>
-    )
+    // return (
+    //     <MobileNavDiv>
+    //         <StyledMobileNavLink to="/your-listings/" activeClassName="active-nav">YOUR LISTINGS</StyledMobileNavLink>
+    //         <StyledMobileNavLink to="/register/" activeClassName="active-nav">REGISTER</StyledMobileNavLink>
+    //         <StyledMobileNavLink to="/" activeClassName="active-nav" isActive={checkActive}>LOGIN</StyledMobileNavLink>
+    //         <StyledMobileNavLink to="/logout/" activeClassName="active-nav">LOGOUT</StyledMobileNavLink>
+    //     </MobileNavDiv>
+    // )
 }
 
 export default Nav
