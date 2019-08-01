a467661d06d188a7662e341bcabe92e430b479b2
Arvin Agas <arvagas@gmail.com>
2019-08-01 13:03:44 -0400

    populate listings with logged in user

--- airbnb-opt-price/src/components/YourListingsCard.js
+++ airbnb-opt-price/src/components/YourListingsCard.js
@@ -13,17 +13,19 @@ const YourListingsCard = (props) => {
         <StyledListingGrid>
             {data.map((listing,index) => (
                 <StyledListingsCard key={index}>
-                    <img src={listing.img_url !== null ? listing.img_url : house} alt='house img'></img>
+                    <img src={house} alt='house img'></img>
+                    {/* <img src={listing.img_url !== null ? listing.img_url : house} alt='house img'></img> */}
                     {/* <img src={((houseImgs[index].webformatURL !== null) && (houseImgs[index].webformatURL !== undefined) && (houseImgs[index].webformatURL !== false) && (houseImgs[index].webformatURL !== NaN)) ? houseImgs[index].webformatURL : house} alt='house img'></img> */}
                     <StyledListingsCardContent>
                         <p className='listing-info'>NEIGHBORHOOD GROUP:</p>
-                        <p>{listing.neighborhood_group}</p>
+                        <p>{listing.neighbourHood.neighbourHoodGroup.name}</p>
                         <p className='listing-info'>NEIGHBORHOOD:</p>
-                        <p>{listing.neighborhood}</p>
-                        <p>GUESTS: {listing.guests}</p>
+                        <p>{listing.neighbourHood.name}</p>
+                        <p>ACCOMMODATES: {listing.accommodates}</p>
                         <p>MINIMUM NIGHTS: {listing.minimum_nights}</p>
-                        <p>ACTUAL PRICE: {listing.act_price}</p>
-                        <p>OPTIMAL PRICE: {listing.opt_price}</p>
+                        {/* <p>ACTUAL PRICE: {listing.act_price}</p> */}
+                        {/* <p>OPTIMAL PRICE: {listing.opt_price}</p> */}
+                        <p>REVIEWS: {listing.number_of_reviews}</p>
                     </StyledListingsCardContent>
                     <StyledLCBC>
                         <YourListingsModal listing={listing} DeleteListing={DeleteListing}>VIEW DETAILS</YourListingsModal>
--- airbnb-opt-price/src/components/YourListingsModal.jsx
+++ airbnb-opt-price/src/components/YourListingsModal.jsx
@@ -19,35 +19,38 @@ class YourListingsModal extends React.Component {
 
         return (
             <Modal open={open} onOpen={this.open} onClose={this.close} trigger={<Button>VIEW DETAILS</Button>} closeIcon>
-                <Modal.Header>{listing.neighborhood}, {listing.neighborhood_group}</Modal.Header>
+                <Modal.Header>{listing.neighbourHood.name}, {listing.neighbourHood.neighbourHoodGroup.name}</Modal.Header>
                 <Modal.Content image scrolling>
-                        <Image wrapped size='medium' src={listing.img_url !== null ? listing.img_url : house} />
+                    <Image wrapped size='medium' src={house} />
+                        {/* <Image wrapped size='medium' src={listing.img_url !== null ? listing.img_url : house} /> */}
                     <Modal.Description>
                         <Header>ADDRESS HERE
-                            <Header.Subheader>Host Listing Number: #</Header.Subheader>
+                            <Header.Subheader>Host Listing Number: {listing.listing_id}</Header.Subheader>
                         </Header>
-                        <p>Cleaning Fee: $#</p>
-                        <p>Security Deposit: $#</p>
-                        <p>Extra People Fee: $#</p>
+                        <p>Cleaning Fee: ${listing.cleaning_fee}.00</p>
+                        <p>Security Deposit: ${listing.security_deposit}.00</p>
+                        <p>Extra People Fee: ${listing.extra_people}.00</p>
                         <hr></hr>
-                        <p>Accomodation Max: #</p>
-                        <p>Guests Allowed: #</p>
+                        <p>Accommodates: {listing.accommodates}</p>
+                        <p>Guests Included: {listing.guests_included}</p>
                         <hr></hr>
-                        <p>Availability: # Days</p>
-                        <p>Room Type: string</p>
-                        <p>Bedrooms: #</p>
-                        <p>Number of Beds: #</p>
-                        <p>Bed Types: #/string</p>
-                        <p>Bathrooms: #</p>
+                        <p>Minumum Nights: {listing.minimum_nights}</p>
+                        <p>Maximum Nights: {listing.maximum_nights}</p>
+                        <hr></hr>
+                        <p>Property: {listing.propertyType.name}</p>
+                        <p>Room Type: {listing.roomType.name}</p>
+                        <p>Bedrooms: {listing.bedrooms}</p>
+                        <p>Bed Types: {listing.bedType.name}</p>
+                        <p>Bathrooms: {listing.bathrooms}</p>
                         <p>Amenities: #</p>
                         <hr></hr>
-                        <p>Cancellation Policy: string</p>
+                        <p>Cancellation Policy: {listing.cancellationPolicy.name.replace(/_/g, ' ')}</p>
                     </Modal.Description>
                 </Modal.Content>
                 <Modal.Actions>
                     <StyledModalUpdateListingsLink as={ Link } to="/update-listing/" onClick={this.close} className='fluid'>UPDATE</StyledModalUpdateListingsLink>
                     <Button negative onClick={() => DeleteListing(listing)}>DELETE</Button>
-                    <Button color='blue' onClick={() => this.close}>CLOSE</Button>
+                    <Button color='blue' onClick={this.close}>CLOSE</Button>
                 </Modal.Actions>
             </Modal>
         )
